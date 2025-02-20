e877b0d009e57ebfc9117c1f5d47144915d1ccc2
dimas <abyad017@gmail.com>
2025-02-20 17:20:43 +0700

    update logout method - remove all local storage

--- src/store/authActions.js
+++ src/store/authActions.js
@@ -2,6 +2,8 @@ import { authAtom } from "@/store/authAtom";
 
 export const logout = (setAuth, router) => {
     localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("username");
     setAuth({ user: null, token: null });
     router.push("/auth/login");
 };
